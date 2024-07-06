3edfda25afb4ea50b51f0b08fcd68075343826db
Banji Owabumoye <owabumoye@gmail.com>
2024-07-06 13:27:51 +0100

    This is my first commit to Test repository

---
+++ rep_test.py
@@ -0,0 +1,13 @@
+def sum (a, b):
+    return a + b
+
+number1 = int(input('enter first number: '))
+number2 = int(input('enter second number: '))
+
+total = sum(number1, number2)
+
+print(f"The sum of the two numbers is: {total}")
+
+
+
+
