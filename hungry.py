4f70deba9fbd729c886d493c3ef1f1947eb65736
Banji Owabumoye <owabumoye@gmail.com>
2024-07-07 09:45:41 +0100

    burger added

--- hungry.py
+++ hungry.py
@@ -5,6 +5,8 @@ if hungry in ['yes','Yes', 'YES']:
     print(f'eat rice')  
     print('eat fries')
     print('eat pizza')  
+    print('eat burger')  
+
 else:
     print(f"have a nice day")
     
