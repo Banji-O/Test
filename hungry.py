7014f08e1e7df3d20eb35e393f12297cd6ed2268
Banji Owabumoye <owabumoye@gmail.com>
2024-07-07 08:47:00 +0100

    more foods added

--- hungry.py
+++ hungry.py
@@ -2,10 +2,11 @@
 hungry = input("are you hungry? ")
 
 if hungry in ['yes','Yes', 'YES']:
-    print(f'eat rice')    
+    print(f'eat rice')  
+    print('eat fries')
+    print('eat pizza')  
 else:
     print(f"have a nice day")
-    
-    
+       
 food = ['yam', 'bread', 'fufu', 'spag']
 
